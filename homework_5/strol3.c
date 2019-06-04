547c40d789b641e42c5a4e895118ab90e34e6329
Maciej Podsedkowski <mpodsedkowski@gmail.com>
2019-06-05 00:38:18 +0100

    PDSC

--- homework_5/strol3.c
+++ homework_5/strol3.c
@@ -3,42 +3,152 @@
 //
 #include <string.h>
 #include <stdbool.h>
+#include <errno.h>
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
 
 
-int decodeValue(char inVal, int base);
 long pow5(long x, int y);
+long charToLong(char input);
 
 
 long
 strtol (const char *nPtr, char **endPtr, int base){
-    errno = 0
+    errno = 0;
 
-    long output;
-    const char *tnPtr = nPtr;
+    long output = 0;
+    //const char *tnPtr = nPtr;
     const char *beginNumber = NULL;
-
-    while ((*nptr = ' ') || (*nptr = '\n') || (*nptr = '\t')){
-        nptr++;
+    const char *endNumber = NULL;
+    const char *startPtr = nPtr;
+    bool negative = false;
+    /*
+    char ** startEndPtr = NULL;
+    if(endPtr){
+        startEndPtr = endPtr;
     }
-    if(base == 0){
-        base = 10;
-        if(*nPtr == 0){
+     */
+
+    while ((*nPtr == ' ') || (*nPtr == '\n') || (*nPtr == '\t')){
+        nPtr++;
+    }
+    if(*nPtr == '-'){
+        negative = true;
+        nPtr++;
+    }
+    if(*nPtr == '+'){
+        nPtr++;
+    }
+    if(base == 0 || base==8 || base==16){ //dodalem warunki bo jak nie wchodzilo to nie przesuwalo nPtr
+        if(base == 0){
+            base = 10;
+        }
+        if(*nPtr == '0'){
             base = 8;
             nPtr++;
             if(*nPtr == 'x'){
                 base = 16;
                 nPtr++;
             }
+            else{
+                if(!(((*nPtr - '0') >= 0) && ((*nPtr - '0') < base))){
+                    if(endPtr){
+                        *endPtr = (char*)nPtr;
+                    }
+                    return 0L;
+                }
+            }
         }
     }
     beginNumber = nPtr;
     if(base < 2 || base > 36){
         errno = EINVAL;
-        *endPtr = (char*)nPtr;
+        if(endPtr){
+            *endPtr = (char*)nPtr;
+        }
         return 0L;
     }
-    //while()
+
+    while((((*nPtr - '0') >= 0) && ((*nPtr - '0') < base) && ((*nPtr - '0') < 10)) || (((*nPtr - 'A') >= 0) && (*nPtr - 'A') < base - 10)){
+        nPtr++;
+    }
+
+    if(endPtr){
+        *endPtr = (char*)nPtr;
+    }  //przesunalem nawias o 2 linie w gore
+    endNumber = nPtr;
+    nPtr--;
+    if(endNumber == beginNumber){
+        if(endPtr) {
+            *endPtr = (char *)startPtr;
+        }
+        return 0L;
+    }
+    //tu trzeba dodać sprawdzenie czy endNumber jest końcem stringa - jak nie to błąd danych wejsiowych
+    //printf(" baza %d ",base);
+
+    while (nPtr >= beginNumber){
+        long bPower = pow5(base, (endNumber - nPtr - 1));
+        //printf(" znak %d potega %d ",(int)charToLong(*nPtr), bPower);
+        if(bPower > 0){   //zawsze jest wieksze od 0
+            if((((LONG_MAX - output) / bPower) < charToLong(*nPtr)) && !negative){ //zmienilem znak nierownosci
+
+                errno = ERANGE;
+                if(endPtr){
+                    *endPtr = (char*) endNumber;
+                }
+                return LONG_MAX;
+
+            }
+        }
+
+        if((charToLong(*nPtr) > 0) && (bPower > 0)) {
+            if (((LONG_MIN  + output) / bPower > -charToLong(*nPtr)) && negative) {  //zmienilem caly zapis
+                errno = ERANGE;
+                if (endPtr) {
+                    *endPtr = (char *) endNumber;
+                }
+                return LONG_MIN;  //zmienilem na min
+            }
+        }
 
 
+        output += charToLong(*nPtr) * pow5(base, (endNumber - nPtr - 1));
+        //printf(" znak %d ",(int)charToLong(*nPtr));
+        nPtr--;
+    }
+    /*
+    if(**endPtr != '\0'){
+        *endPtr = (char*)startPtr;
+        return 0L;
+    }
+     */
+    if(negative){
+        output = -output;
+    }
+
+    return output;
+
+    // return 5;
+
+}
+long pow5(long x, int y) {
+    long final = 1;
+    for (int i = 0; i < y; ++i) {
+        final = final * x; //tu wcześniej powinno być sprawdzenie czy final nie przekracza longmax/x
+    }
+    //printf("pow5 y= %d final= %ld ", y, final);
+    return final;
+}
+
+long charToLong(char input){
+    long output;
+    if(((input - '0') >= 0) && ((input - '0') < 10)){
+        output = input - '0';
+    }
+    if(((input - 'A') >= 0) && ((input - 'A') < 27)){
+        output = input - 'A' + 10;
+    }
     return output;
 }
--- homework_5/strtol.c
+++ homework_5/strtol.c
@@ -6,10 +6,11 @@
 #include <errno.h>
 #include <limits.h>
 #include <math.h>
+#include <stdio.h>
 
 
 long pow5(long x, int y);
-
+long charToLong(char input);
 
 
 long
@@ -20,7 +21,14 @@ strtol (const char *nPtr, char **endPtr, int base){
     //const char *tnPtr = nPtr;
     const char *beginNumber = NULL;
     const char *endNumber = NULL;
+    const char *startPtr = nPtr;
     bool negative = false;
+    /*
+    char ** startEndPtr = NULL;
+    if(endPtr){
+        startEndPtr = endPtr;
+    }
+     */
 
     while ((*nPtr == ' ') || (*nPtr == '\n') || (*nPtr == '\t')){
         nPtr++;
@@ -32,63 +40,123 @@ strtol (const char *nPtr, char **endPtr, int base){
     if(*nPtr == '+'){
         nPtr++;
     }
+    int newBase = base;
     if(base == 0){
-        base = 10;
-        if(*nPtr == '0'){
-            base = 8;
+        newBase = 10;
+    }
+
+    if(*nPtr == '0'){
+        newBase = 8;
+        nPtr++;
+        if(*nPtr == 'x'){
+            newBase = 16;
             nPtr++;
-            if(*nPtr == 'x'){
-                base = 16;
-                nPtr++;
+
+        }
+        else
+            {
+            if(!(((*nPtr - '0') >= 0) && ((*nPtr - '0') < base))){
+                /*
+                if(endPtr){
+                    *endPtr = (char*)nPtr;
+                }
+                return 0L;
+                 */
+                nPtr--;
             }
         }
+    }
+    if(base == 0){
+        base = newBase;
     }
     beginNumber = nPtr;
     if(base < 2 || base > 36){
         errno = EINVAL;
         if(endPtr){
-            *endPtr = (char*)nPtr;
+            *endPtr = (char*)startPtr;
         }
         return 0L;
     }
+
     while((((*nPtr - '0') >= 0) && ((*nPtr - '0') < base) && ((*nPtr - '0') < 10)) || (((*nPtr - 'A') >= 0) && (*nPtr - 'A') < base - 10)){
         nPtr++;
     }
+
     if(endPtr){
         *endPtr = (char*)nPtr;
-        endNumber = nPtr;
-        nPtr--;
+    }  //przesunalem nawias o 2 linie w gore
+    endNumber = nPtr;
+    nPtr--;
+    if(endNumber == beginNumber){
+        if(endPtr) {
+            *endPtr = (char *)startPtr;
+        }
+        return 0L;
     }
+    //tu trzeba dodać sprawdzenie czy endNumber jest końcem stringa - jak nie to błąd danych wejsiowych
+    //printf(" baza %d ",base);
+
     while (nPtr >= beginNumber){
         long bPower = pow5(base, (endNumber - nPtr - 1));
-        if((((LONG_MAX - output) / bPower) > *nPtr) && !negative){
-            errno = ERANGE;
-            if(endPtr){
-                *endPtr = (char*)nPtr;
+        //printf(" znak %d potega %d ",(int)charToLong(*nPtr), bPower);
+        if(bPower > 0){   //zawsze jest wieksze od 0
+            if((((LONG_MAX - output) / bPower) < charToLong(*nPtr)) && !negative){ //zmienilem znak nierownosci
+
+                errno = ERANGE;
+                if(endPtr){
+                    *endPtr = (char*) endNumber;
+                }
+                return LONG_MAX;
+
             }
-            return LONG_MAX;
         }
-        if(((LONG_MIN /(*nPtr * bPower)) > -output) && negative) {
-            errno = ERANGE;
-            if (endPtr) {
-                *endPtr = (char *) nPtr;
+
+        if((charToLong(*nPtr) > 0) && (bPower > 0)) {
+            if (((LONG_MIN  + output) / bPower > -charToLong(*nPtr)) && negative) {  //zmienilem caly zapis
+                errno = ERANGE;
+                if (endPtr) {
+                    *endPtr = (char *) endNumber;
+                }
+                return LONG_MIN;  //zmienilem na min
             }
-            return LONG_MAX;
         }
-        output += *nPtr * (endNumber - nPtr - 1);
+
+
+        output += charToLong(*nPtr) * pow5(base, (endNumber - nPtr - 1));
+        //printf(" znak %d ",(int)charToLong(*nPtr));
         nPtr--;
     }
+    /*
+    if(**endPtr != '\0'){
+        *endPtr = (char*)startPtr;
+        return 0L;
+    }
+     */
     if(negative){
         output = -output;
     }
 
     return output;
+
+   // return 5;
+
 }
 long pow5(long x, int y) {
     long final = 1;
     for (int i = 0; i < y; ++i) {
-        final = final * x;
+        final = final * x; //tu wcześniej powinno być sprawdzenie czy final nie przekracza longmax/x
     }
     //printf("pow5 y= %d final= %ld ", y, final);
     return final;
+}
+
+long charToLong(char input){
+    long output;
+    if(((input - '0') >= 0) && ((input - '0') < 10)){
+        output = input - '0';
+    }
+    if(((input - 'A') >= 0) && ((input - 'A') < 27)){
+        output = input - 'A' + 10;
+    }
+    return output;
 }
